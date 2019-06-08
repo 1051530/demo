d84f3194d181e505c85bb6cfba99d8ddf5a882ed
1051531 <51056061+1051531@users.noreply.github.com>
2019-06-09 03:16:39 +0800

    嗨起來!!!

--- demo/shop/views.py
+++ demo/shop/views.py
@@ -10,9 +10,14 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(request, 'shop/product/list.html', {'category': category, 'catrgories': categories, 'products': products})
+    return render(request, 'shop/product/list.html', {'category': category,
+                                                      'categories': categories, 'products': products})
 
 
 def product_detail(request, id, slug):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, 'shop/product/detail.html', {'product': product})
+    return render(request, 'shop/product/detail.html', {'category': category,
+                                                        'categories': categories, 'product': product})
