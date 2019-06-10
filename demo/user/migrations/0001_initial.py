50895b4ad11d3233a1814c867c17e0492f3b7e1f
1051530 <51055984+1051530@users.noreply.github.com>
2019-06-11 01:16:59 +0800

    購物跟註冊合併囉~

---
+++ demo/user/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('name', models.CharField(max_length=128, unique=True)),
+                ('password', models.CharField(max_length=256)),
+                ('email', models.EmailField(max_length=254, unique=True)),
+                ('common_store', models.CharField(max_length=128)),
+            ],
+        ),
+    ]
