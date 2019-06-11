4c511dee4d711924e195fbecd712b6a28c43f445
1051531 <51056061+1051531@users.noreply.github.com>
2019-06-12 03:08:39 +0800

    6

--- demo/orders/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Order
-
-
-class OrderCreateForm(forms.ModelForm):
-    class Meta:
-        model = Order
-        fields = ['姓', '名', 'email', '地址',
-                  '郵遞區號', '城市']
